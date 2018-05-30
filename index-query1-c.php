9e7a9a4abfc89013864d0c07a276bceb1af219ea
AlamCahya <alamcahya3211@gmail.com>
2018-05-30 21:20:46 +0700

    Paginate dan Navbar

--- index-query1-c.php
+++ index-query1-c.php
@@ -11,24 +11,55 @@
 	<!-- <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script> -->
 </head>
 <body id="body-index">
-	<nav class="navbar navbar-expand-sm fixed-top" id="navbar" style="padding: 0;">
-		<ul class="navbar-nav">
-        <li><a class="" href="index.php" id="home">Home</a></li>
-        <li><a class="" href="evelist.php" id="evlist">Event List</a></li>
-        <li><a class="" href="myprof.php" id="myprof">My Profile</a></li>
-        <li>
-        	<a class="nav-link dropdown-toggle active" href="#" id="navbardrop" data-toggle="dropdown">Query Menus</a>
-      		<div class="dropdown-menu">
-        		<a class="dropdown-item" href="trigger.php">Trigger</a>
-        		<a class="dropdown-item" href="function.php">Function</a>
-        		<a class="dropdown-item" href="proc.php">Procedure</a>
-        		<a class="dropdown-item" href="view.php">View</a>
-        		<a class="dropdown-item" href="join.php">Join</a>
-        		<a class="dropdown-item" href="cursor.php">Cursor</a>
-      		</div>
-        </li>
-		</ul>
-	</nav>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand" href="index.php"><img src="img/standev.png" height="35px"></a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="activestate">
+	        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="evelist.php">Event List</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="myprof.php">My Profile</a>
+	      </li>
+	      <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          Query Menus
+	       </a>
+	       <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+	          <a class="dropdown-item" href="trigger.php">Trigger</a>
+	          <a class="dropdown-item" href="function.php">Function</a>
+	          <a class="dropdown-item" href="proc.php">Procedure</a>
+	          <a class="dropdown-item" href="index-query.php">Index</a>
+	          <a class="dropdown-item" href="view.php">View</a>
+	          <a class="dropdown-item" href="join.php">Join</a>
+	          <a class="dropdown-item" href="cursor.php">Cursor</a>         
+	       </div>
+	      </li>
+      <li class="nav-item">
+	    <?php if(isset($_SESSION['username'])) : ?>
+			<div style="display: flex; margin-left: 80%; color: white;margin-top: 1%;">
+				<p>Welcome! <?php echo $_SESSION['username'] ?></p>
+				<a href="logout.php"><img src="img/logout.png" height="35px"></a>
+			</div>
+		<?php elseif(isset($_COOKIE['username']) && $_COOKIE['username']!='') : ?>
+			<li class="nav-item"> Welcome! <?php echo $_COOKIE['username'] ?>
+				<a href="logout.php" class="btn btn-danger">Logout</a>
+			</li>
+		<?php else: ?>
+			<li class="nav-item">
+				<a href="index.php" class="btn btn-danger">Login</a>
+			</li>
+		<?php endif; ?>
+	   </li>
+    </ul>
+  </div>
+</nav>
 	<div id="session">
 	<?php if(isset($_SESSION['username'])) : ?>
 		<div style="display: flex; margin-left: 80%; color: white;margin-top: 1%;">
@@ -52,14 +83,16 @@
           if($link == false){
             die("ERROR: Could not connect. " . mysqli_connect_error());
           }
-
+          $page = (isset($_GET['page']))? $_GET['page'] : 1;
+		  $limit = 4;
+		  $limit_start = ($page - 1) * $limit;
+		  $sql = "SELECT * FROM post LIMIT ";
+		  $query = $link->query($sql.$limit_start.",".$limit);
           $value = $_POST['index1'];
           $sql = "SELECT post_date, post_id, uevent_id, post_title, post_place, post_desc FROM post 
           where post_date like '%$value%' or post_id like '%$value%' or uevent_id like '%$value%' or post_title like '%$value%' or post_place like '%$value%' or post_desc like '%$value%' ORDER BY post_date;";
-          
-          $result = $link->query($sql);
-          
-        ?>
+          $result = $link->query($sql);       
+    ?>
   		<div class="container">
   			<h1 style="position: center; margin-top: 1%; color: white; text-shadow: 2px 2px black;" class="font-weight-bold">Index I</h1>
   			<a href="index-query.php" class="btn btn-danger">Back</a>
