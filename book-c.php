43874bd599c4c4c1bda501d8f0d859ab352d4de2
yoshimaputri <yoshimashima@gmail.com>
2018-05-31 14:48:20 +0700

    hmm udah slesei

--- book-c.php
+++ book-c.php
@@ -21,6 +21,15 @@
 		$date = date('Y-m-d');
 		$_SESSION['book']=true;
 		$db->query("INSERT INTO booking VALUES('{$bookid}','{$userid['user_id']}','{$_POST['postid']}','{$_POST['type']}','{$_POST['size']}','{$date}','1','0','{$target_file}','file_get_contents($tmp)','0')");
+		$db->query("UPDATE booking
+		JOIN post ON booking.`post_id` = post.`post_id`
+		join user on user.user_id = booking.user_id and user.user_name = {$userid['user_name']}
+		JOIN size ON booking.`size_id` = size.`size_id`
+		JOIN detil_harga ON size.`size_id` = detil_harga.`size_id` 
+		JOIN menambah ON booking.`book_id` = menambah.`book_id`
+		JOIN item ON menambah.`item_id` = item.`item_id`
+		AND post.`post_id` = detil_harga.`post_id`
+		SET booking.`book_totalharga` = (booking.`book_quantity` * detil_harga.`price`) + item.`item_price`;");
 		header('Location: myprof.php?success');
 		exit();
 	}
